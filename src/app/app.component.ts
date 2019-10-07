ca0556c99836953b8dda30d0183b1573557e3b7a
lmazet <lmazet@sigma.fr>
2019-10-07 13:29:46 +0200

    test PIXI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+declare var PIXI: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
+  @ViewChild('pixiContainer', {static: true}) 
+  pixiContainer;
+
+  public pApp: any;
+  ngOnInit(): void {
+    this.pApp = new PIXI.Application({ width: 800, height: 600 }); // this creates our pixi application
+    this.pixiContainer.nativeElement.appendChild(this.pApp.view);
+  }
+
   title = 'blueprintnotincluded';
 }
